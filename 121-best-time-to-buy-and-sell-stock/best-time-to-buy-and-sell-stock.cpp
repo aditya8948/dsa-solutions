7531a4398d305f6d201e4eb68dee8edd13b52771
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2025-12-26 16:41:32 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (25.06%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,10 +4,9 @@ public:
         int minPrice = prices[0];
         int maxProfit = 0;
         for(int i = 1 ; i < prices.size() ;i++){
-            minPrice = min(prices[i] , minPrice);
-            if(maxProfit < prices[i] - minPrice){
-                maxProfit = prices[i] - minPrice;
-            } 
+            minPrice = min(prices[i] , minPrice);        // update buying price  
+            maxProfit = max(maxProfit, prices[i] - minPrice); // update max profit value
+            
         }
         return maxProfit;
     }
