09000ce8bd79efb8c7b8a9c7be880f67cf13fb0d
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-01-28 16:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (65.83%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
-    int checkPartition(vector<int> &nums){
-        int low  = 0 ; int high = nums.size()-1;
-        int n = high ;
-        while(low <= high){
+    // This function finds the pivot index (largest element)
+    int checkPartition(vector<int> &nums) {
+        int low = 0;
+        int high = nums.size() - 1;
+        int lastIdx = high;   // index of last element (reference)
+
+        while (low <= high) {
             int mid = low + (high - low) / 2;
-             if(nums[mid] <= nums[n])  high=mid-1;
-             else if(mid != n && nums[mid] > nums[mid+1]) return mid;
-             else low = mid +1;
+
+            // mid lies in second sorted part
+            if (nums[mid] <= nums[lastIdx]) {
+                high = mid - 1;
+            }
+            // mid lies in first part
+            // check if this is the pivot
+            else if (mid != lastIdx && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            // move towards right
+            else {
+                low = mid + 1;
+            }
+        }
+        return -1; // array is not rotated
+    }
+    
+    int binarySearch(vector<int> &nums, int low, int high, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) high = mid - 1;
+            else low = mid + 1;
         }
         return -1;
     }
 
-    int binarySearch(vector<int> &nums , int low , int high , int target){
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == target) return mid;
-            else if( nums[mid] > target) high = mid - 1;
-            else low = mid +1;
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        int pivot = checkPartition(nums);
+        int pivotIndex = checkPartition(nums);
 
-        if(pivot == -1){
-           return  binarySearch(nums,0,nums.size()-1,target);
+        //  array is not rotated
+        if (pivotIndex == -1) {
+            return binarySearch(nums, 0, n - 1, target);
         }
-        else if(target <= nums[n-1]){
-           return   binarySearch(nums,pivot+1,nums.size()-1,target);
+        //target lies in right sorted part
+        else if (target <= nums[n - 1]) {
+            return binarySearch(nums, pivotIndex + 1, n - 1, target);
         }
-        else{
-          return    binarySearch(nums,0,pivot,target);
+        //target lies in left sorted part
+        else {
+            return binarySearch(nums, 0, pivotIndex, target);
         }
-        
     }
 };
