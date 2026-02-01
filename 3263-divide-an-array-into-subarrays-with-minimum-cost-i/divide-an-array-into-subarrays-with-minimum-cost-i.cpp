49f7d0f17c697347826d30b0b699c0672a2ee980
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-02-01 18:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 30.4 MB (74.87%) - LeetSync

---
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int firstMin = INT_MAX;
+        int secondMin = INT_MAX;
+
+        // find two smallest elements after index 0
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < firstMin) {
+                secondMin = firstMin;
+                firstMin = nums[i];
+            } else if (nums[i] < secondMin) {
+                secondMin = nums[i];
+            }
+        }
+
+        return nums[0] + firstMin + secondMin;
+    }
+};
