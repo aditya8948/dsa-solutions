b502c91938fb36667999666770ef2c40c4b63b7d
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-02-21 17:54:36 +0530

    Time: 132 ms (6.94%) | Memory: 43.3 MB (5.09%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int countBit(int num){
-        long mask = 1L<<31;
+    bool countBit(int num){
+        long mask = 1L<<22;
         int count = 0;
         while(mask){
             if(num & mask) count++;
             mask>>=1;
         }
-        return count ;
-    }
-      bool isPrime(int num){
-        if(num < 2) return false;
-        for(int i = 2; i * i <= num; i++){
-            if(num % i == 0) return false;
+        // value can be only 20  bit so only 1 to 20 number are possible
+        vector<int>prime = {2,3,5,7,11,13,17,19};
+        for(int i = 0 ; i < prime.size() ;i++){
+            if(count == prime[i]) return true;
         }
-        return true;
+        return  false ;
     }
+    // check prime 
+
+    //   bool isPrime(int num){
+    //     if(num < 2) return false;
+    //     for(int i = 2; i * i <= num; i++){
+    //         if(num % i == 0) return false;
+    //     }
+    //     return true;
+    // }
 
 
     int countPrimeSetBits(int left, int right) {
         int bitCount =  0;
         while(left <= right){
-          bool prime = isPrime(countBit(left));
+          bool prime = countBit(left);
           if(prime) bitCount++;
           left++;
         }
