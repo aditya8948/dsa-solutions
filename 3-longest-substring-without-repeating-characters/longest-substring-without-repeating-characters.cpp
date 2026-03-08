e2b3001643c0f2acfcecb9f3aba9c40eb9f2d772
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-03-08 23:20:41 +0530

    Time: 4 ms (71.95%) | Memory: 11.6 MB (81.08%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-    
-    // check substring
-    bool isValid(string sub, char ch) {
-        for(int i = 0; i < sub.size(); i++) {
-            if(sub[i] == ch)
-                return false;
-        }
-        return true;
-    }
-
     int lengthOfLongestSubstring(string s) {
-        int maxLen = 0;
+        int size = s.size();
+        if(s == "") return 0;
+        if(size == 1 ) return 1;
+    vector<int> freq(265,0);
+    int length = 0;
+    int ans = INT_MIN;
+    int i = 0 , j= 0;
 
-        for(int i = 0; i < s.size(); i++) {
-            string subString = "";
-            subString += s[i];     
-            int len = 1;
-
-            for(int j = i + 1; j < s.size(); j++) {
-                if(isValid(subString, s[j])) {
-                    subString += s[j];
-                    len++;
-                } else {
-                    break;
-                }
-            }
-            maxLen = max(maxLen, len);
+    while(j < s.size()){
+        if(freq[s[j]] == 0){
+            freq[s[j]]++;
+            length++;
+            j++;
         }
-        return maxLen;
+        else {
+            freq[s[i]]--;
+            i++;
+            length--;
+        }
+          ans = max(ans , length );
+    }
+    return ans;
     }
 };
