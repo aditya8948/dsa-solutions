39f4decbab776594b5b662b6088412c96844854d
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-04-21 18:14:47 +0530

    Time: 1908 ms (37.32%) | Memory: 580.8 MB (38.98%) - LeetSync

---
+++ 336-palindrome-pairs/palindrome-pairs.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+
+    bool ispal(string& s , int i , int j){
+        while(i <= j){
+            if(s[i] != s[j]) return false ;
+            i++,j--;
+        }
+        return true ;
+    }
+
+
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        unordered_map<string,int> umap;
+        map<pair<int,int> ,bool> m;
+        vector<vector<int>> res;
+        
+        // inserting the words in map in reverse order 
+        for(int i = 0 ; i < words.size() ; i++){
+            umap[string(words[i].rbegin() , words[i].rend())] = i;
+        }
+
+        for(int i = 0 ; i < words.size() ; i++){
+            int n = words[i].size();
+            // for empty string 
+            if(umap.find("") != umap.end() 
+               && umap[""] != i
+               && ispal(words[i], 0, n-1)
+               && m.find({i , umap[""]}) == m.end()
+               && m.find({umap[""], i}) == m.end()
+               ){
+                  m[{i, umap[""]}] = true;
+                  m[{umap[""], i}] = true;
+                  res.push_back({i , umap[""]});
+                  res.push_back({umap[""] , i});
+               }
+
+               // prefix substring 
+
+               for(int j = 0 ; j < n; j++){
+                 string prefix = words[i].substr(0 , j+1);
+                 if(umap.find(prefix) != umap.end()
+                    && umap[prefix] != i
+                    && ispal(words[i] , j+1 , words[i].size()-1)
+                    && m.find({i , umap[prefix]}) == m.end()
+                    ){
+                        m[{i , umap[prefix]}] = true ;
+                        res.push_back({i , umap[prefix]});
+                    }
+               }
+
+               // suffix substrings 
+
+               for(int j = n-1 ; j >= 0 ; j--){
+                string suffix = words[i].substr(j, n-j);
+                 if(umap.find(suffix) != umap.end()
+                    && umap[suffix] != i
+                    && ispal(words[i] , 0 , j-1)
+                    && m.find({umap[suffix] , i}) == m.end()
+                    ){
+                        m[{umap[suffix] , i}] = true ;
+                        res.push_back({umap[suffix] , i});
+                    }
+
+               }
+         }
+        return res;
+    }
+};
