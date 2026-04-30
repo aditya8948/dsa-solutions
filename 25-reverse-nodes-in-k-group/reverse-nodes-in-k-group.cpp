3fe215d8a829cd1b6e3d1832b49626727aa79ce8
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-05-01 01:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (37.78%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+// reverse the chunck
+    ListNode* reverse(ListNode* head , int k  , ListNode* &remainder){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* tmp;
+        int count = 0 ;
+        while(curr && count < k){
+             tmp = curr->next;
+             curr->next = prev;
+             prev = curr;
+             curr = tmp ;
+             count++;
+        }
+        remainder = curr;
+        return prev;
+    }
+
+// recursive code 
+
+    ListNode* kRev(ListNode* head , int k , int totalCount ){
+        if(!head || totalCount < k ) return head ; // termination condition
+
+        // reverse the chunck
+        ListNode* remainder;
+        ListNode* newHead= reverse(head , k , remainder);
+        head->next = kRev(remainder, k , totalCount-k);        
+        return newHead;
+
+    }
+
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        int totalCount = 0 ;
+        ListNode* cur = head ;
+        while(cur){
+             totalCount++;
+             cur = cur ->next;
+        }
+
+        return kRev(head, k, totalCount);
+    }
+};
