f35d80bcb155dc3aa93ed0d8f029b8617b90a211
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-06-02 00:50:02 +0530

    Time: 4 ms (40.84%) | Memory: 55 MB (84.78%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            // swap unitl no. is valid no not get  to its orignal position
-            while(nums[i] <= n && nums[i] >= 1 && nums[i]!= nums[nums[i] - 1]) { 
-                 swap(nums[i], nums[nums[i] - 1]);
+        
+        // put all the positive number at there indexed position -1 
+        for(int i = 0; i < n; i++){
+            while(nums[i] <= n && nums[i] >= 1 && nums[i] != nums[nums[i] - 1]){
+                swap(nums[i] , nums[nums[i] - 1]);
             }
+         }
 
-        }
-        // check which value is not eual to i+1(positive integer)
-        for(int i = 0 ; i < nums.size() ; i++){
+        // first first +ve no we is not present in nums is our ans else n+1
+        for(int i = 0 ; i < n; i++) {
             if(nums[i] != i+1) return i+1;
         }
+        
         return n+1;
     }
 };
