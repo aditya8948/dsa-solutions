b7c9d436d9e7d39226437d694a36b46909ffd579
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-06-29 03:03:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (62.62%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-
-        vector<int> res(nums1.size(), -1);
-
-        for(int i = 0; i < nums1.size(); i++) {
-
-            int index = -1;
-
-            for(int j = 0; j < nums2.size(); j++) {
-                if(nums2[j] == nums1[i]) {
-                    index = j;
-                    break;
-                }
+        int n1 = nums1.size(), n2 = nums2.size();
+        
+        // precomputation of NGE for nums2
+        stack<int> stk;
+        unordered_map<int, int> mp;
+        stk.push(nums2[0]);
+        for(int i = 1; i < n2; i++){
+            while( !stk.empty() && nums2[i] > stk.top()){
+                mp[stk.top()] = nums2[i];
+                stk.pop();
             }
-
-            for(int k = index + 1; k < nums2.size(); k++) {
-                if(nums2[k] > nums1[i]) {
-                    res[i] = nums2[k];
-                    break;
-                }
-            }
+            stk.push(nums2[i]);
         }
+        while(!stk.empty()){
+            mp[stk.top()] = -1;
+            stk.pop();
+        }
+       
 
-        return res;
+       // result
+        vector<int>res(n1);
+       for(int i = 0; i < n1; i++){
+            res[i] = mp[nums1[i]];
+       }
+       return res;
     }
 };
