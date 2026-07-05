2776ed56cd8d8fa0efdbc7b3511b32eef8baed9f
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-07-05 13:41:30 +0530

    Time: 4 ms (28.58%) | Memory: 8.3 MB (45.94%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t mask = 1;
+        uint32_t ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            if (mask & n)
+                ans |= 1;
+            mask <<= 1;
+        }
+
+        return ans;
+    }
+};
