c72defc6d888483f8a2cac2fc29fa9c7ec6a2df5
aditya8948 <154893500+aditya8948@users.noreply.github.com>
2026-07-16 19:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (87.82%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,39 +10,20 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* head = NULL;
-        ListNode* tail = NULL;
-        ListNode* l1 = list1;
-        ListNode* l2 = list2;
+    ListNode* merge(ListNode* l1, ListNode* l2) {
+        if (!l1) return l2;
+        if (!l2) return l1;
 
-        while(l1 && l2){
-            ListNode* tmp;
-            if(l1->val < l2->val){
-               tmp = l1;
-               l1 = l1->next;
-            }
-            else{
-                tmp = l2;
-                l2 = l2->next;
-            }
-
-            if(!head){
-                head = tmp;
-                tail = tmp;
-            }
-            else{
-                tail->next = tmp;
-                tail = tail->next;
-            }
-           
+        if (l1->val < l2->val) {
+            l1->next = merge(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = merge(l1, l2->next);
+            return l2;   
         }
-        // return if head is null / either one or bith list are empty
-        if (!head) return l1 ? l1 : l2;
-        
-        // bind the remaining list
-        if(l1) tail->next = l1;
-        if(l2) tail->next = l2;
-        return head;
+    }
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        return merge(list1, list2);
     }
 };
